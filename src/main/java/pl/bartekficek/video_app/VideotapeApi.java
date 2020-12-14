2e2d85be2afae1c8ca9dd107c1758d7e774a14c4
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-14 21:31:30 +0100

    methods in getAllVideotapes & getVideotepeById added

--- src/main/java/pl/bartekficek/video_app/VideotapeApi.java
+++ src/main/java/pl/bartekficek/video_app/VideotapeApi.java
@@ -1,12 +1,17 @@
 package pl.bartekficek.video_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/videotapes")
 public class VideotapeApi {
 
     private List<Videotape> videotapes;
@@ -16,6 +21,17 @@ public class VideotapeApi {
         videotapes.add(new Videotape(1L, "Titanic", LocalDate.of(1995,1,1)));
         videotapes.add(new Videotape(2L, "Matrix", LocalDate.of(2000,3,5)));
         videotapes.add(new Videotape(3L, "Shrek", LocalDate.of(2005,12,7)));
+    }
+
+    @GetMapping("/all")
+    public List<Videotape> getAllVideotapes() {
+        return videotapes;
+    }
+    
+    @GetMapping
+    public Videotape getVideotapeById(@RequestParam int id){
+        Optional<Videotape> first = videotapes.stream().filter(tape -> tape.getId() == id).findFirst();
+        return first.get();
     }
 
 }
