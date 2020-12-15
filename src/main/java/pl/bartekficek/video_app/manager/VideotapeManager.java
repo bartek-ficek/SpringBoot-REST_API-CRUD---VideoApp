18e9177b0e9301d0c0877701f207b581440b8a40
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-15 14:22:05 +0100

    implemented CRUD methods in Videotape manager

--- src/main/java/pl/bartekficek/video_app/manager/VideotapeManager.java
+++ src/main/java/pl/bartekficek/video_app/manager/VideotapeManager.java
@@ -1,7 +1,35 @@
 package pl.bartekficek.video_app.manager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.bartekficek.video_app.dao.VideoTapeRepo;
+import pl.bartekficek.video_app.dao.entity.Videotape;
+
+import java.util.Optional;
 
 @Service
 public class VideotapeManager {
+
+    private VideoTapeRepo videotapeRepo;
+
+    @Autowired
+    public VideotapeManager(VideoTapeRepo videotapeRepo) {
+        this.videotapeRepo = videotapeRepo;
+    }
+
+    public Iterable<Videotape> findAllVideotapes() {
+        return videotapeRepo.findAll();
+    }
+
+    public Optional<Videotape> findVideotapeById(Long id) {
+        return videotapeRepo.findById(id);
+    }
+
+    public Videotape saveVideotape(Videotape videotape) {
+        return videotapeRepo.save(videotape);
+    }
+
+    public void deleteVideotape(Long id) {
+        videotapeRepo.deleteById(id);
+    }
 }
