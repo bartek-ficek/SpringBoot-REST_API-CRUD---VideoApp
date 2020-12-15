4ee7615dc6735a299dd881944f3e2f0e4324b31e
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-15 11:43:29 +0100

    added & checked method updateVideoTape(), deleteVideoTape() in Api

--- src/main/java/pl/bartekficek/video_app/VideotapeApi.java
+++ src/main/java/pl/bartekficek/video_app/VideotapeApi.java
@@ -15,25 +15,36 @@ public class VideotapeApi {
 
     public VideotapeApi() {
         videotapes = new ArrayList<>();
-        videotapes.add(new Videotape(1L, "Titanic", LocalDate.of(1995,1,1)));
-        videotapes.add(new Videotape(2L, "Matrix", LocalDate.of(2000,3,5)));
-        videotapes.add(new Videotape(3L, "Shrek", LocalDate.of(2005,12,7)));
+        videotapes.add(new Videotape(1L, "Titanic", LocalDate.of(1995, 1, 1)));
+        videotapes.add(new Videotape(2L, "Matrix", LocalDate.of(2000, 3, 5)));
+        videotapes.add(new Videotape(3L, "Shrek", LocalDate.of(2005, 12, 7)));
     }
 
     @GetMapping("/all")
     public List<Videotape> getAllVideotapes() {
         return videotapes;
     }
-    
+
     @GetMapping
-    public Videotape getVideotapeById(@RequestParam int id){
+    public Videotape getVideotapeById(@RequestParam int id) {
         Optional<Videotape> first = videotapes.stream().filter(tape -> tape.getId() == id).findFirst();
         return first.get();
     }
 
     @PostMapping("/add")
-    public boolean addVideotape(@RequestBody Videotape videotape){
+    public boolean addVideotape(@RequestBody Videotape videotape) {
         return videotapes.add(videotape);
     }
 
+    @PutMapping("/update")
+    public void updateVideotape(@RequestParam int id, @RequestBody Videotape videotape) {
+        Videotape videotapeNewData = videotapes.stream().filter(tape -> tape.getId() == id).findFirst().get();
+        videotapeNewData.setProductionYear(videotape.getProductionYear());
+        videotapeNewData.setTitle(videotape.getTitle());
+    }
+
+    @DeleteMapping("/delete")
+    public boolean deleteVideotape(@RequestParam int id) {
+        return videotapes.removeIf(videotape -> videotape.getId() == id);
+    }
 }
